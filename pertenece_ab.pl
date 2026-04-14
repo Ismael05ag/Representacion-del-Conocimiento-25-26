84d769f5a058941ef1293220fe5db912e04a10c1
Ismael05ag <ismael.aliano@alu.uhu.es>
2026-04-14 19:04:53 +0200

    Creación de pertenece_ab.pl

---
+++ pertenece_ab.pl
@@ -0,0 +1,13 @@
+arbol1( a(1, a(2, nil, nil), a(3, nil, a(4, nil, nil))) ).
+
+/*
+pertenece_ab(+Elem, +ArbolBinario)
+	es cierto si la etiqueta Elem pertenece a ArbolBinario.
+*/
+
+pertenece_ab(Elem, a(Elem, _, _)).
+
+pertenece_ab(Elem, a(_, ArbolIzq, _)):- pertenece_ab(Elem, ArbolIzq). 
+
+pertenece_ab(Elem, a(_, _, ArbolDer)):- pertenece_ab(Elem, ArbolDer).
+					    
